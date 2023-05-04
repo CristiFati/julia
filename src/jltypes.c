04ce07abe055b4bf06d8525764116d5acc8b3517
Alex Arslan <ararslan@comcast.net>
2023-05-04 10:28:14 -0700

    Define a method for `hash(::Type, ::UInt)`
    Currently, `hash(::Type, ::UInt)` uses `objectid`, which can have some
    odd behavior for types: in particular, subsequent identical type-valued
    variable definitions can have `objectid`s which differ from the first
    such definition. This has some bizarre downstream effects when e.g.
    using types as the values of a `Set` or the keys of a `Dict`. See issue
    49620 for examples.
    
    There is an internal `type_hash` C function used for caching types but
    isn't exposed to Julia, as Jameson pointed out in the linked issue. This
    commit exposes it as `jl_type_hash` which is then used via `ccall` to
    define a method `hash(::Type, ::UInt)`. This method then fixes #49620.
    Note, however, that this does not affect the differing `objectid`s for
    otherwise identical types.

--- src/jltypes.c
+++ src/jltypes.c
@@ -1575,6 +1575,12 @@ static unsigned type_hash(jl_value_t *kj, int *failed) JL_NOTSAFEPOINT
     }
 }
 
+JL_DLLEXPORT uintptr_t jl_type_hash(jl_value_t *v) JL_NOTSAFEPOINT
+{
+    int *failed = 0;
+    return type_hash(v, failed);
+}
+
 static unsigned typekey_hash(jl_typename_t *tn, jl_value_t **key, size_t n, int nofail) JL_NOTSAFEPOINT
 {
     if (tn == jl_type_typename && key[0] == jl_bottom_type)
